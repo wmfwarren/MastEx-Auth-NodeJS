465963dc9a7244b635312d75d762190c2e1f2285
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-30 11:58:08 -0500

    working on login

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,30 @@
 "use strict";
 
 const User = require("../models/user.js")
+const bcrypt = require("bcrypt");
 
-module.exports.create = () => {
-  
+module.exports.create = (req, res) => {
+  const inputEmail = req.body.email;
+  const inputPassword = req.body.password;
+
+  User.findOne({email: inputEmail})
+  .then ((user) => {
+    console.log(user);
+    if(user){
+      console.log("IF DONE GONE FIRED");
+      bcrpyt.compare(inputPassword, user.password, (err, matches) => {
+        console.log("COMPARE DONE GONE FIRED");
+        if(matches) {
+          req.session.email = inputEmail;
+          res.send({msg: "Good'ay Mate"});
+        } else {
+          res.send({msg: "You messed it up"});
+        }
+
+      })
+      .catch(err);
+    } else {
+      res.send({msg: "No such email"});
+    }
+  })
 }
