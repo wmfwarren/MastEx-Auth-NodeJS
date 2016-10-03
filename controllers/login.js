80b8dbf1663d842561397bcc247fcf9bd2f60b3a
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-10-03 14:00:59 -0500

    fixed typo

--- controllers/login.js
+++ controllers/login.js
@@ -12,30 +12,30 @@ module.exports.create = (req, res) => {
     console.log(user);
     if(user){
       console.log("IF DONE GONE FIRED");
-      console.log("USERPW", user.password);
+      console.log("USER", user);
       console.log("Typed PW", inputPassword);
       return new Promise ((resolve, reject) => {
       console.log("Promise DONE GONE FIRED");
-        bcrpyt.compare(inputPassword, user.password, (err, matches) => {
+        bcrypt.compare(inputPassword, user.password, (err, matches) => {
           console.log("Compare fired");
           if(err) {
             reject(err);
           } else {
             resolve(matches);
-          }
-        })
-      })
+          } //end of if else
+        }) //end of compare method
+      }) //end of new promise
       .then((matches) => {
         if(matches){
-          session.email = inputEmail;
+          // session.email = inputEmail;
           res.send({msg: "Good News everybody"});
         } else {
           res.send({msg: "you meesed up something, try again!"});
-        }
-      })
-      .catch(err)
+        } //end of if else
+      }) //end of the then for the mathing promise
+      .catch(err) //catch bcrpyt.comapre errs
   } else {
       res.send({msg: "No such email"});
-    }
-  })
-}
+    } //end of else form if(user)
+  }) //end of the findOne.then
+} //end of module.exports
